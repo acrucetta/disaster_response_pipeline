2a88e941dc0cfa2f1ae834d1ce5b48201c0030f7
acrucetta <acrucettanieto@gmail.com>
2020-05-12 11:24:56 -0500

    Updated train classifier's model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,85 @@
-import sys
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_roc_curve
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+import nltk
+nltk.download(['punkt', 'wordnet','stopwords'])
+from nltk.corpus import stopwords
+from sklearn.externals import joblib
 
 
 def load_data(database_filepath):
-    pass
-
+    df = pd.read_sql_table(database_filepath, engine)
+    X = df['message']
+    y = df.iloc[:, 4:40]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    return X_train, X_test, y_train, y_test
 
 def tokenize(text):
-    pass
+    # creating stop words
+    stop_words = set(stopwords.words("english"))
+
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # tokenize text
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    # lemmatize and remove stop words
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+
+    return tokens
 
 
 def build_model():
-    pass
+        pipeline = Pipeline([
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())])),
+            ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+
+        parameters = {
+            'clf__estimator__max_depth': [2, 5],
+            'clf__estimator__n_estimators': [50, 100]
+        }
+        cv = GridSearchCV(pipeline, param_grid=parameters)
+        return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    model = build_model()
+    model.fit(X_test, Y_test)
 
+    y_pred = model.predict(X_test)
+    for i in category_names:
+        n = 0
+        print(i)
+        print('Random Forest with Grid Search')
+        print(classification_report(Y_test[i], y_pred[:, n], labels=[0, 1]))
+        n += 1
+
+    return y_pred
 
 def save_model(model, model_filepath):
-    pass
+    joblib.dump(model, model_filepath)
 
 
 def main():
